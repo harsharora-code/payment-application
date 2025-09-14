1c04a1fd88dd30631dcfd7858feb3e66856f2452
Harsh Arora <harshpunyani20@gmail.com>
2025-09-14 12:26:06 +0530

    change in fe

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,6 +1,15 @@
+import { Users } from "../components/Users"
+import { Balance } from "../components/Balance"
+import { Appbar } from "../components/Appbar"
 export default function DashBoard() {
     return (
-        <>
-        </>
+        <div>
+            <Appbar/>
+            <div className="m-8">
+                <Balance value={"10,000"} />  
+                <Users/>
+            </div>
+        
+        </div>
     )
 }
